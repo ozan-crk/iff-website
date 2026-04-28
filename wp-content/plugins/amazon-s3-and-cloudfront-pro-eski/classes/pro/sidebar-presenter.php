878877ea85100280fd9f354e28afcc91eaf87416
Ozan Cırık <263227724+ozan-crk@users.noreply.github.com>
2026-04-29 02:29:19 +0300

    fix:

--- wp-content/plugins/amazon-s3-and-cloudfront-pro-eski/classes/pro/sidebar-presenter.php
+++ wp-content/plugins/amazon-s3-and-cloudfront-pro-eski/classes/pro/sidebar-presenter.php
@@ -242,5 +242,5 @@ class Sidebar_Presenter {
 	/**
 	 * As this class is a singleton it should not be able to be unserialized.
 	 */
-	protected function __wakeup() {}
+	public function __wakeup() {}
 }
