1adb6b31d4f44026e6c83a0c8783d60129895453
Ozan Cırık <263227724+ozan-crk@users.noreply.github.com>
2026-04-29 01:41:14 +0300

    Update functions.php

--- wp-content/themes/iff-theme/functions.php
+++ wp-content/themes/iff-theme/functions.php
@@ -91,3 +91,12 @@ function iff_add_font_mime_types($mimes) {
 }
 
 
+add_filter('use_block_editor_for_post_type', function($use_block_editor, $post_type) {
+    if ($post_type === 'post') {
+        return false; // yazılar klasik editör
+    }
+    if ($post_type === 'page') {
+        return true; // sayfalar Gutenberg
+    }
+    return $use_block_editor;
+}, 10, 2);
