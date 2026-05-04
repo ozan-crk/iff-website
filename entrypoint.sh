071fc025c18eee31cdefb0582e3d82baed7a1378
Ozan Cırık <263227724+ozan-crk@users.noreply.github.com>
2026-05-04 06:29:59 +0300

    fix: session

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,10 @@ echo "----------------------------------------------------"
 
 # wp-content Senkronizasyonu (Git'ten gelen güncel dosyaları kalıcı volume'a aktar)
 echo ">> wp-content dosyaları senkronize ediliyor..."
-mkdir -p /var/www/html/wp-content
+mkdir -p /var/www/html/wp-content/sessions
 cp -ru /usr/src/wp-content-source/. /var/www/html/wp-content/
 chown -R www-data:www-data /var/www/html/wp-content
+chmod 777 /var/www/html/wp-content/sessions
 
 
 echo "----------------------------------------------------"
